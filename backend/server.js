4baf698268076e57ab7bc3ea4bd61a2bcf51a7df
“RameshPrashanth98” <prashanthdharmadasa6698@gmail.com>
2022-03-19 01:32:04 +0530

    modified server.js

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,40 @@
 const express = require('express');
-
-
-const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require ('dotenv');
+const app = express();
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+//Mongodb connection
 const dbConfig = require('./db');
-
-const roomsRoute = require('./routes/rooms');
-
-//Api 
-app.use('/api/rooms',roomsRoute)
-
 
 
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+}));
+app.use(bodyParser.json());
+
+app.get("/test", (req, res) => {
+    res.send("It works"); 
+});
+
+app.use(express.json());
+app.use(cookieParser());
+ 
+
 app.listen(PORT,()=>{
     console.log(`Server running on PORT: ${PORT}`)
 })
+
+
+//Api for room management Ramesh 
+const roomsRoute = require('./routes/rooms');
+app.use('/api/rooms',roomsRoute);
 
 
 
