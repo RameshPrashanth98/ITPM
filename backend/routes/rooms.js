3da33a9c6f0f357f1d5757ec987861963cedb7df
“RameshPrashanth98” <prashanthdharmadasa6698@gmail.com>
2022-03-19 01:31:06 +0530

    Add route for add room for system

--- backend/routes/rooms.js
+++ backend/routes/rooms.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+
 const Rooms = require("../models/rooms") 
 
 
@@ -15,7 +16,7 @@ router.get("/getallrooms",async(req,res)=>{
 });
 
 //Add new room to the system
-/*router.post("/addnewroom",async(req,res)=>{
+router.post("/addnewroom",async(req,res)=>{
 
     try {
         const newroom = new Rooms(req.body)
@@ -25,10 +26,47 @@ router.get("/getallrooms",async(req,res)=>{
     } catch (error) {
         return res.status(400).json({error});
     }
+})
+/*router.route("/addroom").post((req,res)=>{
+    const roomname = req.body.roomname;
+    const noOfguests = req.body.noOfguests;
+    const roomtype = req.body.roomtype;
+    const facilities = req.body.facilities;
+    const rentperday = req.body.rentperday;
+    const description = req.body.description;
+    const imageurl1 = req.body.imageurl1;
+    const imageurl2 = req.body.imageurl2;
+    const imageurl3 = req.body.imageurl3;
+    const imageurls = [imageurl1, imageurl2, imageurl3];
+
+ 
+
+    const newRoom =  new Rooms({ 
+        roomname,
+        noOfguests,
+        roomtype,
+        facilities,
+        rentperday,
+        description,
+        imageurls,
+        
+
+    })
+
+    //pass room object to mongodb database(Create function)
+    newRoom.save().then(()=>{
+        //function execute if new room details added to the database
+        res.json("New room sucesfully added to the system")
+
+    }).catch((err)=>{
+        //If error occurs this fuction execute
+        console.log(err);
+    });
+
 })*/
 
  //Update room details
- /*router.route("/update/:id").put(async(req,res)=>{
+ /*router.route("/updateroom/:id").put(async(req,res)=>{
   let roomID = req.params.id;  //get unique user id from data base
 
     const updateroom = new Rooms(req.body);
@@ -36,7 +74,7 @@ router.get("/getallrooms",async(req,res)=>{
     const update = await Rooms.findByIdAndUpdate(roomID,updateroom ).then(()=>{
         res.status(200).send({status: "room details are updated"});
     }).catch((e)=>{
-     //console.log.(err.message);
+     
         console.log(e);
         res.status(500).send({status:"Error in updating room datails"})
     })
@@ -47,12 +85,12 @@ router.get("/getallrooms",async(req,res)=>{
 
 
    //Delete room details from database
-   router.route("/delete/:id").delete(async(req,res)=>{
+   router.route("/deleteroom/:id").delete(async(req,res)=>{
         
     let roomID = req.params.id;
 
     await Rooms.findByIdAndDelete(roomID).then(()=>{
-        res.status(200).send({status: "room deleted from database"});
+        res.status(200).send({status: "room deleted from system"});
     }).catch((e)=>{
         console.log(e);
            res.status(500).send({status:"Error in deleting room datails"})
